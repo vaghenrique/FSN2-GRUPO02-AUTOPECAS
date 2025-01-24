1e02d74e3cb201a0451554aebd5992bc7bfa063c
Matheus Fontinele <fonttydiv@gmail.com>
2025-01-24 11:08:52 -0300

    Configuração do Router ReactJS(botões de rediricionamento e html de site n estão configurados)

--- projeto_site/src/App.js
+++ projeto_site/src/App.js
@@ -1,177 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import home from "./pages/home";
+import catalogo from "./pages/catalogo";
+import devolucao from "./pages/devolucao";
+import elements from "./pages/elements";
+import fale from "./pages/fale";
+import instrucoes from "./pages/instrucoes";
+import localizacao from "./pages/localizacao";
+import pagamento from "./pages/pagamento";
+import parceiros from "./pages/parceiros";
+import produto1 from "./pages/produto1";
+import produto2 from "./pages/produto2";
+import produto3 from "./pages/produto3";
+import produto4 from "./pages/produto4";
+import produto5 from "./pages/produto5";
+import produto6 from "./pages/produto6";
+import produto7 from "./pages/produto7";
+import produto8 from "./pages/produto8";
+import produto9 from "./pages/produto9";
+import produto10 from "./pages/produto10";
+import produto11 from "./pages/produto11";
+import produto12 from "./pages/produto12";
+import produto13 from "./pages/produto13";
+import produto14 from "./pages/produto14";
+import produto15 from "./pages/produto15";
+import produto16 from "./pages/produto16";
+import produto17 from "./pages/produto17";
+import produto18 from "./pages/produto18";
+import produto19 from "./pages/produto19";
+import produto20 from "./pages/produto20";
+import produto21 from "./pages/produto21";
+import produto22 from "./pages/produto22";
+import produto23 from "./pages/produto23";
+import produto24 from "./pages/produto24";
+import produto25 from "./pages/produto25";
+import produto26 from "./pages/produto26";
+import produto27 from "./pages/produto27";
+import produto28 from "./pages/produto28";
+import produto29 from "./pages/produto29";
+import produto30 from "./pages/produto30";
 
 function App() {
   return (
-    <div className="is-preload">
-
-        <div id="wrapper">
-
-            <div id="main">
-              <div className="inner">
-
-                  <header id="header">
-                    <a className="logo"><strong>AutoPrime</strong></a>
-                    <ul className="icons">
-                      <li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
-                      <li><a href="#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
-                      <li><a href="#" className="icon brands fa-snapchat-ghost"><span className="label">Snapchat</span></a></li>
-                      <li><a href="#" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
-                      <li><a href="#" className="icon brands fa-medium-m"><span className="label">Medium</span></a></li>
-                    </ul>
-                  </header>
-
-                  <section id="banner">
-                    <div className="content">
-                      <header>
-                        <h1>AutoPrime</h1>
-                      </header>
-                      <p>Fundada em 2024, a AutoPrime
-                        surgiu com o objetivo de oferecer
-                        autopeças de qualidade, preços
-                        justos e entregas rápidas,
-                        atendendo às necessidades tanto
-                        de motoristas quanto de oficinas
-                        mecânicas. Desde o início, a
-                        empresa apostou na tecnologia,
-                        criando um sistema de pedidos
-                        online e atendimento via aplicativo,
-                        destacando-se pela inovação e
-                        eficiencia.</p>
-                      <ul className="actions">
-                        <li><a href="localizacao.html" className="button big">Clique aqui</a></li>
-                      </ul>
-                    </div>
-                    <span className="image object">
-                      <img src="images/farois.jpeg" alt="" />
-                    </span>
-                  </section>
-
-                  <section>
-                    <header className="major">
-                      <h2>Erat lacinia</h2>
-                    </header>
-                    <div className="features">
-                      <article>
-                        <span className="icon fa-gem"></span>
-                        <div className="content">
-                          <h3>Eficiencia</h3>
-                          <p>Combinamos tecnologia avançada e processos otimizados para oferecer uma experiência ágil e prática.</p>
-                        </div>
-                      </article>
-                      <article>
-                        <span className="icon solid fa-paper-plane"></span>
-                        <div className="content">
-                          <h3>Qualidade</h3>
-                          <p>Na AutoPrime, qualidade não é apenas uma promessa, é um compromisso. </p>
-                        </div>
-                      </article>
-                      <article>
-                        <span className="icon solid fa-rocket"></span>
-                        <div className="content">
-                          <h3>Inovação</h3>
-                          <p>Estamos sempre à frente no mercado de autopeças, trazendo soluções modernas para facilitar sua vida.</p>
-                        </div>
-                      </article>
-                      <article>
-                        <span className="icon solid fa-signal"></span>
-                        <div className="content">
-                          <h3>Atendimento</h3>
-                          <p>Mais do que clientes, queremos parceiros. Nosso time de especialistas está preparado para oferecer suporte técnico, tirar dúvidas e garantir que você tenha a melhor experiência de compra.</p>
-                        </div>
-                      </article>
-                    </div>
-                  </section>
-
-                  <section>
-                    <header className="major">
-                      <h2>Veja tambem nossos produtos</h2>
-                    </header>
-                    <div className="posts">
-                      <article>
-                        <a className="image"><img src="images/amortecedor.jpeg" alt="" /></a>
-                        <h3>Amortecedorn</h3>
-                        <ul className="actions">
-                          <li><a href="produto20.html" className="button">Clique Aqui</a></li>
-                        </ul>
-                      </article>
-                      <article>
-                        <a className="image"><img src="images/bico_injetor.jpg" alt="" /></a>
-                        <h3>Bico Injetor</h3>
-                        <ul className="actions">
-                          <li><a href="produto1.html" className="button">Clique Aqui</a></li>
-                        </ul>
-                      </article>
-                    </div>
-                  </section>
-
-              </div>
-            </div>
-
-            <div id="sidebar">
-              <div className="inner">
-
-                  <nav id="menu">
-                    <header className="major">
-                      <h2>Menu</h2>
-                    </header>
-                    <ul>
-                      <li><a href="index.html">Tela Inicial</a></li>
-                      <li><a href="catalogo.html">Catalago</a></li>
-                      <li><a href="localizacao.html">Localização</a></li>
-                      <li><a href="parceiros.html">Parceiros</a></li>
-                      <li><a href="devolucao.html">Devolução</a></li>
-                      <li><a href="redesociais.html">Rede Sociais</a></li>
-                      <li><a href="instrucoes.html">Instruções</a></li>
-                      <li><a href="pagamento.html">Formas de Pagamento</a></li>
-                      <li><a href="fale.html">Fale Comigo</a></li>
-                    </ul>
-                  </nav>
-
-                  <section>
-                    <header className="major">
-                      <h2>Produtos</h2>
-                    </header>
-                    <div className="mini-posts">
-                      <article>
-                        <a href="produto2.html" className="image"><img src="images/pic01-4.png" alt="" /></a>
-                      </article>
-                      <article>
-                        <a href="produto10.html" className="image"><img src="images/pic01-Recuperado1.png" alt="" /></a>
-                      </article>
-                      <article>
-                        <a href="produto19.html" className="image"><img src="images/radiador.jpeg" alt="" /></a>
-                      </article>
-                    </div>
-                    <ul className="actions">
-                      <li><a href="catalogo.html" className="button">More</a></li>
-                    </ul>
-                  </section>
-
-                  <section>
-                    <header className="major">
-                      <h2>Get in touch</h2>
-                    </header>
-                    <p>Sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin sed aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-                    <ul className="contact">
-                      <li className="icon solid fa-envelope"><a href="mailto:emailalgum@gmail.com">emailalgum@gmail.com</a></li>
-                      <li className="icon solid fa-phone">(012) 345-6789</li>
-                      <li className="icon solid fa-home">1234 Rua lugar algum N°5678<br />
-                      Fortaleza, CEP 09876-5432</li>
-                    </ul>
-                  </section>
-
-                  <footer id="footer">
-                    <p className="copyright">&copy; Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
-                  </footer>
-
-              </div>
-            </div>
-
-        </div>
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<home />} />
+        <Route path="/catalogo" element={<catalogo />} />
+        <Route path="/devolucao" element={<devolucao />} />
+        <Route path="/elements" element={<elements />} />
+        <Route path="/fale" element={<fale />} />
+        <Route path="/instrucoes" element={<instrucoes />} />
+        <Route path="/localizacao" element={<localizacao />} />
+        <Route path="/pagamento" element={<pagamento />} />
+        <Route path="/parceiros" element={<parceiros />} />
+        <Route path="/p1" element={<produto1 />} />
+        <Route path="/p2" element={<produto2 />} />
+        <Route path="/p3" element={<produto3 />} />
+        <Route path="/p4" element={<produto4 />} />
+        <Route path="/p5" element={<produto5 />} />
+        <Route path="/p6" element={<produto6 />} />
+        <Route path="/p7" element={<produto7 />} />
+        <Route path="/p8" element={<produto8 />} />
+        <Route path="/p9" element={<produto9 />} />
+        <Route path="/p10" element={<produto10 />} />
+        <Route path="/p11" element={<produto11 />} />
+        <Route path="/p12" element={<produto12 />} />
+        <Route path="/p13" element={<produto13 />} />
+        <Route path="/p14" element={<produto14 />} />
+        <Route path="/p15" element={<produto15 />} />
+        <Route path="/p16" element={<produto16 />} />
+        <Route path="/p17" element={<produto17 />} />
+        <Route path="/p18" element={<produto18 />} />
+        <Route path="/p19" element={<produto19 />} />
+        <Route path="/p20" element={<produto20 />} />
+        <Route path="/p21" element={<produto21 />} />
+        <Route path="/p22" element={<produto22 />} />
+        <Route path="/p23" element={<produto23 />} />
+        <Route path="/p24" element={<produto24 />} />
+        <Route path="/p25" element={<produto25 />} />
+        <Route path="/p26" element={<produto26 />} />
+        <Route path="/p27" element={<produto27 />} />
+        <Route path="/p28" element={<produto28 />} />
+        <Route path="/p29" element={<produto29 />} />
+        <Route path="/p30" element={<produto30 />} />
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ projeto_site/src/pages/produto24.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function produto24() {
+  return (
+    <div>
+      <h1>Bem-vindo ao meu site</h1>
+      <p>Esta é a página inicial.</p>
+    </div>
+  );
+}
+
+export default produto24;
